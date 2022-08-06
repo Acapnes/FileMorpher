1a808fedc09f8dbfa34d5ae14ff8e6723e9495f9
Alper <61701011+Acapnes@users.noreply.github.com>
2022-08-06 10:26:02 +0300

    helpers

---
+++ src/helpers/logging.js
@@ -0,0 +1,19 @@
+const { ConnectandList } = require("../commands/commands");
+const { ListFileTablesAndFolderNames, ListSubFoldersImages } = require("./pathHelper");
+
+
+
+
+async function ConsoleLogger() {
+    //console.log("")
+    //console.log("--------------- FileTables and Folders -------------");
+    //console.log(ListFileTablesAndFolderNames())
+    //console.log("")
+    //console.log("--------------- Connected Tables -------------");
+    //ConnectandList();
+    // console.log("")
+    // console.log("--------------- ListSubFolder Images ------------")
+    //ListSubFoldersImages();
+}
+
+module.exports = ConsoleLogger;
