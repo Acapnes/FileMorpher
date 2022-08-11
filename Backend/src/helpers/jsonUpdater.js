b19ef4360d0a10fa231a3105f384571219f196af
Alper <61701011+Acapnes@users.noreply.github.com>
2022-08-12 00:17:01 +0300

    v3

--- Backend/src/helpers/jsonUpdater.js
+++ Backend/src/helpers/jsonUpdater.js
@@ -20,16 +20,39 @@ function ConfigUpdater(ConnString, Database, Userid, Password) {
 
         fs.writeFileSync(SettingsConfig.DirectoryPaths.ConfigPath, json, 'utf-8')
 
-        return "Updated Succesfully"
+        return true
     }
     catch (err) {
         console.log("Config Update Error!! " + err)
-        return "Something went wrong please try again"
+        return false
+    }
+
+}
+
+function PathsUpdater(FileTable, Folder, Index) {
+
+    try {
+        let json = fs.readFileSync(SettingsConfig.DirectoryPaths.PathsPath, "utf-8");
+
+        let logs = JSON.parse(json);
+
+        console.log(logs.TablePaths[Index])
+
+        // json = JSON.stringify(logs);
+
+        // fs.writeFileSync(SettingsConfig.DirectoryPaths.ConfigPath, json, 'utf-8')
+
+        return true
+    }
+    catch (err) {
+        console.log("Config Update Error!! " + err)
+        return false
     }
 
 }
 
 
 module.exports = {
-    ConfigUpdater
+    ConfigUpdater,
+    PathsUpdater
 }
