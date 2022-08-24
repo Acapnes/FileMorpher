b36e5c479a8a270e6dc3f45292523fe4c1a22ae8
Alper <61701011+Acapnes@users.noreply.github.com>
2022-08-24 13:22:06 +0300

    Create pathUpdater.js

---
+++ Backend/src/helpers/jsonUpdater/pathUpdater.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const SettingsConfig = require("../../config.json");
+const { AddLog } = require("../../logging/log");
+
+
+/// Path.json FileTable,FolderName,Status update etme fonksiyonu
+function PathsUpdater(
+  FileTable,
+  FolderName,
+  Status,
+  SelectedDate,
+  CopyOrCut,
+  OverWrite,
+  Procedure,
+  Index
+) {
+  try {
+    let json = fs.readFileSync(
+      SettingsConfig.DirectoryPaths.PathsPath,
+      "utf-8"
+    );
+
+    let logs = JSON.parse(json);
+
+    logs.TablePaths[Index].FileTable = FileTable;
+    logs.TablePaths[Index].FolderName = FolderName;
+    logs.TablePaths[Index].Status = Status;
+    logs.TablePaths[Index].SelectedDate = SelectedDate;
+    logs.TablePaths[Index].CopyOrCut = CopyOrCut;
+    logs.TablePaths[Index].OverWrite = OverWrite;
+    logs.TablePaths[Index].Procedure = Procedure;
+    json = JSON.stringify(logs);
+
+    fs.writeFileSync(SettingsConfig.DirectoryPaths.PathsPath, json, "utf-8");
+
+    return true;
+  } catch (err) {
+    AddLog("Path updating error! " + err);
+    return false;
+  }
+}
+
+function PathsStatusUpdater(Status, Index) {
+  try {
+    let json = fs.readFileSync(
+      SettingsConfig.DirectoryPaths.PathsPath,
+      "utf-8"
+    );
+
+    let logs = JSON.parse(json);
+
+    logs.TablePaths[Index].Status = Status;
+    json = JSON.stringify(logs);
+
+    fs.writeFileSync(SettingsConfig.DirectoryPaths.PathsPath, json, "utf-8");
+
+    return true;
+  } catch (err) {
+    AddLog("Path updating error! " + err);
+    return false;
+  }
+}
+
+/// path.json yeni path ekleme
+function PathAdd(FileTable, FolderName) {
+  try {
+    let json = fs.readFileSync(
+      SettingsConfig.DirectoryPaths.PathsPath,
+      "utf-8"
+    );
+
+    let logs = JSON.parse(json);
+
+    logs.TablePaths.push({
+      FileTable: FileTable,
+      FolderName: FolderName,
+      Status: false,
+      SelectedDate: "2",
+      CopyOrCut: false,
+      OverWrite: false,
+      Procedure: "null",
+    });
+
+    json = JSON.stringify(logs);
+
+    fs.writeFileSync(SettingsConfig.DirectoryPaths.PathsPath, json, "utf-8");
+
+    return true;
+  } catch (err) {
+    AddLog("Path adding error! " + err);
+    return false;
+  }
+}
+
+module.exports = {
+  PathsUpdater,
+  PathAdd,
+  PathsStatusUpdater,
+};
